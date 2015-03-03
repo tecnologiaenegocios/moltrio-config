9d091d348f8eeecbdd0c9100ab1bb3e622fff5fd
Renato Zannon <zannon@tn.com.br>
2015-03-03 19:18:44 -0300

    Primitive configuration api

--- lib/moltrio/config.rb
+++ lib/moltrio/config.rb
@@ -11,6 +11,26 @@ require_relative 'config/scoped_config'
 
 module Moltrio
   module Config
-    # Your code goes here...
+    def self.configure
+      yield self
+      freeze
+    end
+
+    def self.storage
+      @storage ||= :filesystem
+    end
+
+    VALID_STORAGES = %i(etcd filesystem)
+    def self.storage=(storage)
+      unless VALID_STORAGES.include?(storage)
+        raise "#{storage} is not valid. Pleases select one of #{VALID_STORAGES}"
+      end
+
+      @storage = storage
+    end
+
+    def self.storage_options
+      @storage_options ||= {}
+    end
   end
 end
