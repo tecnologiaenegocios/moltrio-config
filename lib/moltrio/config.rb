c0c49e74de9f7103951a177975b853f0dedd7815
Renato Zannon <zannon@tn.com.br>
2015-03-03 18:49:42 -0300

    Bring in the requires

--- lib/moltrio/config.rb
+++ lib/moltrio/config.rb
@@ -1,4 +1,13 @@
-require "moltrio/config/version"
+require 'thread_attr_accessor'
+
+require_relative "config/version"
+
+require_relative 'config/listener'
+require_relative 'config/storage_chain'
+require_relative 'config/file_storage'
+require_relative 'config/etcd_storage'
+require_relative 'config/database_yml_storage'
+require_relative 'config/scoped_config'
 
 module Moltrio
   module Config
