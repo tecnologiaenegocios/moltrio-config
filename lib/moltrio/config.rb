86c76020fefa7ff5ff89930ece8c3382ae74ea02
Renato Zannon <zannon@tn.com.br>
2015-03-16 20:45:48 -0300

    Opt-in chain caching

--- lib/moltrio/config.rb
+++ lib/moltrio/config.rb
@@ -10,24 +10,29 @@ module Moltrio
     extend self
 
     def configure(&block)
-      builder = Builder.run(&block)
-
-      @chains = builder.chains
-      @config = builder.config
-
+      @configuration_block = block
       self
     end
 
     extend ThreadAttrAccessor
     thread_attr_accessor :current_namespace, inherit: true, private: true
-    thread_attr_accessor :active_chains, inherit: true, private: true,
-      default: -> { @chains }
+    thread_attr_accessor :cached_chains, inherit: true, private: true
+
+    def enable_caching
+      self.cached_chains ||= {}
+    end
+
+    def disable_caching
+      self.cached_chains = nil
+    end
 
     def switch_to_namespace(namespace)
       self.current_namespace = namespace
-      self.active_chains = @chains.map { |chain_name, chain|
-        [chain_name, chain.on_namespace(namespace)]
-      }
+
+      if cached_chains
+        cached_chains.delete(:namespaced)
+      end
+
       self
     end
 
@@ -38,15 +43,47 @@ module Moltrio
     end
 
     def chain(name)
-      chain = active_chains[name]
+      chain = namespaced_chains[name]
 
       if chain.nil?
         raise "No chain named #{name.inspect} configured!"
-      elsif chain.requires_namespace? && !current_namespace
+      elsif chain.requires_namespace?
         raise "Chain #{name.inspect} requires namespace, but no namespace provided"
       else
         chain
       end
+    end
+
+  private
+
+    def root_chains
+      return cached_chains[:root] if cached_chains && cached_chains[:root]
+
+      chains = Builder.run(&@configuration_block).chains
+
+      if cached_chains
+        cached_chains[:root] = chains
+      end
+
+      chains
+    end
+
+    def namespaced_chains
+      return cached_chains[:namespaced] if cached_chains && cached_chains[:namespaced]
+
+      if current_namespace
+        chains = root_chains.map { |chain_name, chain|
+          [chain_name, chain.on_namespace(current_namespace)]
+        }
+      else
+        chains = root_chains
+      end
+
+      if cached_chains
+        cached_chains[:namespaced_chains] = chains
+      end
+
+      chains
     end
   end
 end
