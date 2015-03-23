a3181b1040ce3975df864b105f5e5bfb2b1ac6fe
Renato Zannon <zannon@tn.com.br>
2015-03-23 14:56:13 -0300

    Refactor (and optimize) Scoped

--- lib/moltrio/config/adapters/scoped.rb
+++ lib/moltrio/config/adapters/scoped.rb
@@ -19,8 +19,9 @@ module Moltrio
       class << self
         def delegate_to_base_scoping_first_argument(*delegations)
           delegations.each do |method|
-            define_method(method) do |*args, &block|
-              base.public_send(method, *scope_first_argument(args), &block)
+            define_method(method) do |key, *args, &block|
+              key = scope_key(key)
+              base.public_send(method, key, *args, &block)
             end
           end
         end
@@ -30,17 +31,13 @@ module Moltrio
         self.class.new(self, scope)
       end
 
-      delegate_to_base_scoping_first_argument :[], :[]=, :has_key?, :fetch
+      delegate_to_base_scoping_first_argument :[], :[]=, :has_key?
 
     private
       attr_reader :base, :scope
 
-      def scoped_key(key)
+      def scope_key(key)
         "#{scope}.#{key}"
-      end
-
-      def scope_first_argument(args)
-        args.unshift(scoped_key(args.shift))
       end
     end
   end
